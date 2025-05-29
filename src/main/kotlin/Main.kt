59c4b6946f1c236c2edd58dd3e3330995f2bb100
Nikola Srdoc <nikola.srdoc@gmail.com>
2025-05-29 20:23:27 +0200

    Section 2 exercise 13 done

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -9,17 +9,31 @@ import kotlinx.coroutines.runBlocking
 
 fun main() {
 
-runBlocking {
-    val job1=launch {
-        delay(3000L)
-        println("Job1 launched")
-    }
-    job1.invokeOnCompletion { println("Job 1 completed") }
-    delay(500L)
+    runBlocking {
+        val job1 = launch {
+           // delay(3000L)
+            println("Job1 launched")
+            val job2 = launch {
 
-    println("Job 1 will be cancelled")
-    job1.cancel()
-}
+                println("Job 2 launched")
+                delay(3000L)
+                println("Job 2 is finishing")
+            }
+            job2.invokeOnCompletion { println("Job2 completed") }
+
+            val job3 = launch {
+                println("job3 launched")
+                delay(3000L)
+                println("job3 is finishing")
+            }
+            job3.invokeOnCompletion { println("Job3 completed") }
+        }
+        job1.invokeOnCompletion { println("Job 1 completed") }
+        delay(500L)
+
+        println("Job 1 will be cancelled")
+        job1.cancel()
+    }
 
 }
 /*
